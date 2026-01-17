be4881fef79d410e7707141fa54e49a3385d9602
ArtemDljnk <artemodessa31.01@gmail.com>
2026-01-17 21:20:21 +0200

    2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import './App.css'
 import {useState} from "react";
 import type {ITodo} from "./models/ITodo.ts";
+import {todos as dbTodos} from "./data/todos.ts";
+import {Todos} from "./components/todos-component/Todos.tsx";
 
 function App() {
-  const [todos, setTodos] = useState<ITodo[]>()
+  const [todos, setTodos] = useState<ITodo[]>(dbTodos)
+  const deleteTodoHandler = (id: number) => {
+    const filterTodos = todos.filter(todo => todo.id !== id);
+    setTodos(filterTodos);
+  }
   return (
     <>
-      
+      <Todos todos={todos} onDelete={deleteTodoHandler} />
     </>
   )
 }
